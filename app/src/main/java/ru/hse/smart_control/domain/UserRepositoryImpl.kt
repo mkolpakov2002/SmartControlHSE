5a3a77990c26ab2bb23b1c3f07f7aee807e82f9c
tdrykova <tdrykova@edu.hse.ru>
2024-05-13 12:49:58 +0300

    register error handle, auth_time, nav

--- app/src/main/java/ru/hse/smart_control/domain/UserRepositoryImpl.kt
+++ app/src/main/java/ru/hse/smart_control/domain/UserRepositoryImpl.kt
@@ -1,16 +1,51 @@
 package ru.hse.smart_control.domain
 
 import android.app.Application
+import android.util.Log
+import com.google.common.reflect.TypeToken
+import com.google.gson.Gson
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
 import ru.hse.smart_control.domain.connection.http.nest.RetrofitInstance
 import ru.hse.smart_control.model.repository.UserRepository
 import ru.hse.smart_control.model.user.LoginModel
 import ru.hse.smart_control.model.user.RegisterModel
 import ru.hse.smart_control.model.user.TokenModel
+//import kotlin.Result
 
 class UserRepositoryImpl: UserRepository {
 
-    override suspend fun register(user: RegisterModel) {
-        RetrofitInstance.nestApi.register(user)
+    override suspend fun register(user: RegisterModel): Flow<ApiResponse<Unit>> = flow {
+        try {
+            val response = RetrofitInstance.nestApi.register(user)
+            if (response.isSuccessful) {
+                emit(ApiResponse.Success(response.body()!!))
+            } else {
+                val gson = Gson()
+                val type = object : TypeToken<ApiResponseError>() {}.type
+                val errorResponse: ApiResponseError = gson.fromJson(response.errorBody()!!.charStream(), type)
+                emit(ApiResponse.Failure(response.code(), errorResponse.message.joinToString("\n")))
+            }
+        } catch (e: Exception) {
+//        } catch (e: HttpException) {
+            Log.d("TAG", "register:  ${e.message}")
+            emit(ApiResponse.Failure(null, "error"))
+        }
+    }
+
+    override suspend fun register2(user: RegisterModel): Result<RegisterModel, DataError.Network> {
+        return try {
+            val response = RetrofitInstance.nestApi.register(user)
+            Result.Success(user)
+        } catch(e: HttpException) {
+            when(e.code()) {
+                408 -> Result.Error(DataError.Network.REQUEST_TIMEOUT)
+                409 -> Result.Error(DataError.Network.SUCH_USERNAME_EMAIL_EXIST)
+                413 -> Result.Error(DataError.Network.PAYLOAD_TOO_LARGE)
+                else -> Result.Error(DataError.Network.UNKNOWN)
+            }
+        }
     }
 
     override suspend fun loginUser(user: LoginModel): TokenModel {
--- app/src/main/java/ru/hse/smart_control/model/repository/UserRepository.kt
+++ app/src/main/java/ru/hse/smart_control/model/repository/UserRepository.kt
@@ -1,5 +1,9 @@
 package ru.hse.smart_control.model.repository
 
+import kotlinx.coroutines.flow.Flow
+import ru.hse.smart_control.domain.ApiResponse
+import ru.hse.smart_control.domain.DataError
+import ru.hse.smart_control.domain.Result
 import ru.hse.smart_control.model.user.LoginModel
 import ru.hse.smart_control.model.user.RegisterModel
 import ru.hse.smart_control.model.user.TokenModel
@@ -7,7 +11,9 @@ import ru.hse.smart_control.model.user.TokenModel
 interface UserRepository {
 
     // Network
-    suspend fun register(user: RegisterModel)
+    suspend fun register(user: RegisterModel): Flow<ApiResponse<Unit>>
+    suspend fun register2(user: RegisterModel): Result<RegisterModel, DataError.Network>
+
     suspend fun loginUser(user: LoginModel): TokenModel
 
 }
